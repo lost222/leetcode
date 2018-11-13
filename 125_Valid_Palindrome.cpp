15f9fcfec4f5f3555c0b0ebd664d92180ffc77df
lost222 <1610299@mail.nankai.edu.cn>
2018-11-13 16:07:55 +0800

    c

--- 125_Valid_Palindrome.cpp
+++ 125_Valid_Palindrome.cpp
@@ -21,11 +21,14 @@
     
 /* 2. 基于c11语法， remove掉不在乎的， 直接revese了看一样吗。 基于string == 深度重载。 */
 // 注意自定义函数的名字瞎写比较好，如果和重定义的重名，会不知道用哪个
+//
 static bool issmys(char c){
    return !isalnum(c);
 }
 bool isPalindrome(string s) {
     transform(s.begin(), s.end(), s.begin(), ::tolower);
+ //s.erase(remove_if(s.begin(), s.end(), ::isalnum), s.end()); 因为using namespace std 的关系， 必须这么写 
+ // https://stackoverflow.com/questions/8364678/g-string-remove-if-error
     s.erase(remove_if(s.begin(), s.end(), issmys), s.end()); // 这里会不知道用哪个  本句是常见的组合拳， 能擦除string里你不想要的内容。
     string t = s;
     reverse(t.begin(), t.end());
